0db923a2e3f02ee67510141c9b5fc56c7cc87972
meteopavel <isu@meteopavel.ru>
2025-04-18 19:17:27 +0800

    add models

--- education/education/settings.py
+++ education/education/settings.py
@@ -37,6 +37,9 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    'courses.apps.CoursesConfig',
+    'api.apps.ApiConfig',
+    'rest_framework',
 ]
 
 MIDDLEWARE = [
